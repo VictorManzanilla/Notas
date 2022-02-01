2f006305706de8cb7152fcec40198ac42482797d
Victor <vmanzanilla7@gmail.com>
2022-01-31 19:40:42 -0600

    moving to register branch

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -3,16 +3,16 @@ import {Link, withRouter} from 'react-router-dom'
 import {getUser, logout} from './helpers'
 
 const Nav = ({history}) => {
-    return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/" as={Link} to="/">Home</a>
-    
-    <div className="collapse navbar-collapse" >
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active"  href="/create" as={Link} to="/create">Create Note</a>
-        </li>
+  return(
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/" as={Link} to="/">Home</a>
+          
+          <div className="collapse navbar-collapse" >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active"  href="/create" as={Link} to="/create">Create Note</a>
+              </li>
         {!getUser() && (
                     <li className="nav-item">
                         <a className="nav-link active" href="/login" as={Link} to="/login">Login</a>
@@ -25,35 +25,12 @@ const Nav = ({history}) => {
                     
                 </li>
                )}
-      </ul>
-    </div>
-  </div>
-</nav>
-
-            
-            // <ul className="nav nav-tabs">
-            //     <li className="nav-item pr-3 pt-3 pb-3">
-            //         <Link to="/">Home</Link>
-            //     </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
                 
-            //     <li className="nav-item pr-3 pt-3 pb-3">
-            //         <Link to="/create">Create Post</Link>
-            //     </li>
-            //    {!getUser() && (
-            //         <li className="nav-item ml-auto pr-3 pt-3 pb-3">
-            //         <Link to="/login">Login</Link>
-            //     </li>
-            //    )}
-            //    {getUser() && (
-            //         <li onClick={() => logout(() => history.push('/'))} className="nav-item ml-auto pr-3 pt-3 pb-3">
-            //         logout
-            //     </li>
-            //    )}
-            // </ul>
-          
-            // </nav>
-        
-    )
+  )
 
 }
 
